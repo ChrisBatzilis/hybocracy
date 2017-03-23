2895cedb07bf5692d1369cdb67dc906a931602bc
Kettunen Pietari, INI-BIX-INO-BDM <Pietari.Kettunen@swisscom.com>
2017-03-23 23:20:11 +0100

    work on collective crud ops

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component'
+import { FooterComponent } from './footer/footer.component';
+import { CollectiveComponent } from './collective/collective.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    CollectiveComponent
   ],
   imports: [
     BrowserModule,
--- server/src/api/index.ts
+++ server/src/api/index.ts
@@ -1,7 +1,9 @@
 import * as express from 'express';
-
+import { collectiveRouter } from './collective';
 export let router = express.Router();
 
 router.get('/version', (req, res) => {
   res.send('vers 0.0.1 - SNAPSHOT');
 });
+
+router.use('/collectives', collectiveRouter);
