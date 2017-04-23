d85a2807e3f363d0c39c9c680735fa37f43e69ce
Kettunen Pietari, INI-BIX-INO-BDM <Pietari.Kettunen@swisscom.com>
2017-04-23 23:10:19 +0200

    style libs added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import { MaterialModule } from '@angular/material';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { CollectiveComponent } from './collective/collective.component';
@@ -16,7 +16,8 @@ import { CollectiveComponent } from './collective/collective.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
