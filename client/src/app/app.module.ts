60ca672719c60111ee3bfabdef7b73a3e68c6b89
Kettunen Pietari, INI-BIX-INO-BDM <Pietari.Kettunen@swisscom.com>
2017-04-23 23:26:27 +0200

    using angular material

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { MaterialModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { CollectiveComponent } from './collective/collective.component';
@@ -17,7 +18,8 @@ import { CollectiveComponent } from './collective/collective.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-    MaterialModule
+    MaterialModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
