cf94932fb52a516e330c283e1eb5394dada04ec0
Batzilis Christos, INI-DEV-DIG-AEL <Christos.Batzilis@swisscom.com>
2017-05-26 09:58:42 +0200

    Added model and controller for members

--- server/src/api/member.ts
+++ server/src/api/member.ts
@@ -1,18 +1,45 @@
 import * as express from 'express';
 import * as _ from 'lodash';
+require('./models/member');
 export let memberRouter = express.Router();
+var mongoose = require('mongoose');
+var Members = mongoose.model('Member');
 
-memberRouter.get('/', (req, res) => {
-  res.json([]);  
-});
+memberRouter.get('/:username', (req, res) =>{
+console.log("Attempt to find member "+req.params.username);
+Members.find({username:req.params.username},{_id:1,username:1,firstName:1,lastName:1,email:1},
+      function(err,member){
+         if(err){
+           sendJsonResponse(res,500,err);
+         }else{
+           sendJsonResponse(res,200,member);
+         }
+    });
+  });
 
-memberRouter.post('/:id', (req, res) => {
+memberRouter.put('/:id', (req, res) => {
   console.log('edit member');
-  res.json({ });  
+  res.json({ });
 });
 
 memberRouter.post('/', (req, res) => {
-  let member = req.body;
-  console.log('saving member:', member);
-  res.json(member);  
+console.log("Attempt to create new member "+req.body.username);
+ var payload=req.body;
+ Members.create(payload, function(err,member){
+    if(err){
+      if(err.code==11000){
+        sendJsonResponse(res,409,err);
+      }else{
+       sendJsonResponse(res,400,err);
+    }
+  }else{
+      sendJsonResponse(res,201,member);
+      }
+ });
 });
+
+
+var sendJsonResponse = function(res,status,content) {
+  res.status(status);
+  res.json(content);
+};
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import {HttpServer} from './httpServer';
 require('./api/models/db');
+
 const debugapi = require('debug')('red0:server');
 
 //debug('starting red0 ui-server');
