2a04921e37f4303a319db32feb642519ccffb96f
Pietari Kettunen <pietari@sqooba.io>
2017-06-14 21:34:05 +0200

    hide coll without login

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Hybocracy!';
+
+  constructor(private authService:AuthService) {}
+
+  loginStatus() {
+    return this.authService.isLoggedIn();
+  }
 }
